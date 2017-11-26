35b47b2d915c0c0e51e5d30a9884da77e4b66634
Atif Iqbal <atif.ahangar@stonybrook.edu>
2017-11-25 21:36:15 -0500

    Part 3 working for simple case, not optimization yet.

---
+++ part3/run_synth.sh
@@ -0,0 +1,7 @@
+
+dc_shell -f runsynth_layer_16_12_8_20.tcl | tee layer_16_12_8_20_synth.rpt
+dc_shell -f runsynth_layer_5_2_5_9.tcl    | tee layer_5_2_5_9_synth.rpt
+dc_shell -f runsynth_layer_4_4_1_16.tcl   | tee layer_4_4_1_16_synth.rpt
+dc_shell -f runsynth_layer_16_12_1_20.tcl | tee layer_16_12_1_20_synth.rpt
+dc_shell -f runsynth_layer_5_2_1_9.tcl    | tee layer_5_2_1_9_synth.rpt
+dc_shell -f runsynth_layer_13_16_1_32.tcl | tee layer_13_16_1_32_synth.rpt
